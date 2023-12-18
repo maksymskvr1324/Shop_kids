356b9ef29e2e68e3f08482707767bc60c00de9aa
Bosco <bosco@Boscos-MacBook-Pro.local>
2023-12-18 12:29:08 +0200

    added p with text

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+        <div>
+            Home     
+        </div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab hic, deleniti dolorem sapiente odit sunt explicabo perspiciatis, in suscipit rem aperiam! Ut suscipit tenetur fugiat! Incidunt distinctio accusantium consequatur fugiat?</p>
+    </>
   )
 }
