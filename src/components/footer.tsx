c1a8852b6ed88f5da8872d271655871ef62a0a80
MaksSrovron <bosco@Boscos-MacBook-Pro.local>
2023-12-20 11:14:50 +0200

    commit fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export function Footer(){
     return (
         <footer>
-            <div className="footer-content">
-              <div className="container">
+            <div className="container">
+              <div className="footer-content">
                     <div className="footer-bottom">
                       <p>&copy; 2023 Ваша компанія. Усі права захищено.</p>
                     </div>
