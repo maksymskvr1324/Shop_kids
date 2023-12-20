322defbb39904269c168bc5e48f9c643bdfc29a9
MaksSrovron <bosco@Boscos-MacBook-Pro.local>
2023-12-20 10:43:51 +0200

    add min footer

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
   return (
     <div>
       <Navbar />
+
       <Footer/> 
-      
     </div>
   );
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,27 +2,12 @@ import React from "react";
 
 export function Footer(){
     return (
-        <footer className="footer">
-            <div className="logo-footer">Kids Shop</div>
+        <footer>
             <div className="footer-content">
-                <div className="">
-                    <ul className="">
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/contact">Contact</a></li>
-                    </ul>
-                </div>
-                <div className="icons">
-                    <div className="icon">j</div>
-                    <div className="icon">j</div>
-                    <div className="icon">j</div>
-                </div>
-                <div className="other">
-                    <ul className="">
-                        <li><a href="/">1</a></li>
-                        <li><a href="/">2</a></li>
-                        <li><a href="/">3</a></li>
-                    </ul>
+              <div className="container">
+                    <div className="footer-bottom">
+                      <p>&copy; 2023 Ваша компанія. Усі права захищено.</p>
+                    </div>
                 </div>
             </div>
         </footer>
