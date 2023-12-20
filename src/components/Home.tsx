fa6ebe86118399ec1a4630cf9dced1845d34e2d8
MaksSrovron <bosco@Boscos-MacBook-Pro.local>
2023-12-20 09:06:45 +0200

    add position icon navbar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,15 +4,15 @@ export  function Navbar() {
   return (
     <nav className="navbar">
       <div className="container">
-        <div className="logo">Kids Shop</div>
+        <div className="logo"><a href="/">Kids Shop</a></div>
         <ul className="nav-links">
           <li><a className='active' href="/">Home</a></li>
           <li><a href="/shop">Shop</a></li>
           <li><a href="/about">About us</a></li>
           <li><a href="/contact">Contact</a></li>
-          <div className='navbar-icon'>
-            <span className="material-symbols-outlined">shopping_cart</span>
-            <span className="material-symbols-outlined">account_circle</span>
+          <div className='navbar-icon'> 
+            <div className='shop'><span className="material-symbols-outlined">shopping_cart</span></div>
+            <div className='account'><span className="material-symbols-outlined">account_circle</span></div>
           </div>
         </ul>
       </div>
