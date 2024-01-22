83c7a6cecf5b01f73c4dea3e917dd383e031bdc1
maksymskvr1324 <maksym.skvr@gmail.com>
2024-01-22 21:44:24 +0200

    корзина

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Basket from './basket';
 
-
 export function Navbar() {
   const [isBasketVisible, setBasketVisible] = useState(false);
+  const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
 
   const toggleBasket = () => {
-    setBasketVisible(!isBasketVisible);}
+    setBasketVisible(!isBasketVisible);
+  };
+
   return (
     <nav className="navbar">
       <div className="container">
@@ -18,24 +20,17 @@ export function Navbar() {
           <CustomLink href="/About_us">About us</CustomLink>
           <CustomLink href="/Contact">Contact</CustomLink>
           <div className='navbar-icon'> 
-            
-          <div className='shop' onClick={toggleBasket}>
+            <div className='shop' onClick={toggleBasket}>
               <span className="material-symbols-outlined">shopping_cart</span>
             </div>
-
             <div className='account'><Link to="/Registr"><span className="material-symbols-outlined">account_circle</span></Link></div>
           </div>
         </ul>
       </div>
-      {isBasketVisible && <Basket />}
+      {isBasketVisible && <Basket cartItems={cartItems} />}
     </nav>
-   
   );
 }
-
-
-
-
 
 interface CustomLinkProps {
   href: string;
